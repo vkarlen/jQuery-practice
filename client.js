405df88031e1acffcea9504967e95809857f7636
Vada Karlen <vada.karlen@gmail.com>
2021-01-28 14:51:26 -0600

    added some button function

--- client.js
+++ client.js
@@ -8,4 +8,20 @@ function onReady() {
   const h2 = $('h2');
 
   $('h2').addClass('froggy');
+
+  $('#submitButton').on('click', addHarmonica);
+  // to call actions on dynamically generated elements
+  $(document).on('click', '.deleteBtn', deleteButton);
+}
+
+function addHarmonica() {
+  console.log('Click!');
+  let brand = $('#brand').val();
+  $('#harmonicas').append(
+    `<li>Brand is: ${brand} <button class="deleteBtn">Delete</button></li>`
+  );
+}
+
+function deleteButton() {
+  console.log('bye bye');
 }
